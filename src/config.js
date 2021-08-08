f88df4df47014abf9814f105ebe425b5a212e103
Carlos Indriago <carlos.indriago@tsoftglobal.com>
2021-08-08 15:03:13 -0300

    Changed api endpoint

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const host = {
-  apiPort: 'http://localhost:8888/api/curryWurst'
+  /* apiPort: 'http://localhost:8888/api/curryWurst' */
+  apiPort: 'https://currywurst2334.herokuapp.com/api/curryWurst'
 };
 
 const docker = {
